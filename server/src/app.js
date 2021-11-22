8842f6f6723e4d4a4789788acbbcceae91672bb8
3zbumban <37989650+3zbumban@users.noreply.github.com>
2021-11-22 22:13:40 +0100

    asd

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const channels = require("./channels");
 
 const authentication = require("./authentication");
 
-const mongodb = require("./mongodb");
+// const mongodb = require("./mongodb");
 
 const app = express(feathers());
 
@@ -41,7 +41,7 @@ app.use("/", express.static(app.get("public")));
 app.configure(express.rest());
 app.configure(socketio());
 
-app.configure(mongodb);
+// app.configure(mongodb);
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
